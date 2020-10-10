0c2a41f91ded4b1e240782ce13b5fd0642b9bf71
Zaman <zamn.info@gmail.com>
2020-10-10 12:17:25 +0600

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div>
+            <Logo />
+            <h1 class="title">
+                NuxtCRUD
+            </h1>
+            <div class="links">
+                <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer" class="button--green">
+                    Documentation
+                </a>
+                <a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer" class="button--grey">
+                    GitHub
+                </a>
+            </div>
+            {{ posts }}
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            posts: []
+        }
+    },
+    async fetch() {
+        this.posts = await this.$axios.$get(
+            'https://jsonplaceholder.typicode.com/posts'
+        )
+    },
+    fetchOnServer: false
+}
+</script>
+
+<style>
+
+</style>
